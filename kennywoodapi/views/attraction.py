96cb7388c4ac16fb273d15a08cf179407b3f0c5c
Michelle Johnson <bob1234girl@gmail.com>
2020-02-19 10:30:19 -0600

    attraction CRUD complete

--- kennywoodapi/views/attraction.py
+++ kennywoodapi/views/attraction.py
@@ -37,6 +37,52 @@ class Attractions(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized Attraction instance
+        """
+        new_attraction = Attraction()
+        new_attraction.name = request.data["name"]
+        new_attraction.area_id = request.data["area_id"]
+        new_attraction.save()
+
+        serializer = AttractionSerializer(new_attraction, context={'request': request})
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a attraction
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        attraction = Attraction.objects.get(pk=pk)
+        attraction.name = request.data["name"]
+        attraction.area_id = request.data["area_id"]
+        attraction.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single attraction
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            attraction = Attraction.objects.get(pk=pk)
+            attraction.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Attraction.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
         """Handle GET requests to park attractions resource
